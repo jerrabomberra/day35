ccd413c066d70408f1d8920c8d7e793090a22f26
Doug Gordon <gordononline@bigpond.com>
2024-06-10 22:01:00 +0700

    using unix timestamp

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 from ISO3166 import ISO3166
-from datetime import datetime
+import datetime
 
 # abloom is at 13.773648654025774, 100.54110921045502
-current_datetime = datetime.now().replace(microsecond=0)
+# current_datetime = datetime.now().replace(microsecond=0)
 OEM_endpoint = 'https://api.openweathermap.org/data/2.5/weather'
 API_KEY: str = '8c928036410e6f5ffc2690b3c94b81b8'
+
+dt = datetime.datetime.fromtimestamp(0).replace(microsecond=0)
 
 weather_params = {
     "lat": 13.773648654025774,
@@ -14,9 +16,10 @@ weather_params = {
     "units": "metric",
 }
 
-response = requests.get(OEM_endpoint, params=weather_params)
+
 
 def print_weather():
+    response = requests.get(OEM_endpoint, params=weather_params)
     if response.status_code == 200:
         data = response.json()
         city = data['name']
@@ -25,7 +28,7 @@ def print_weather():
         desc = data['weather'][0]['description']
         country_code = data['sys']['country']
         country = ISO3166[country_code]
-        print(f'Weather report for: {current_datetime}')
+        print(f'Weather report for: {dt}')
         print(f'Country: {country}')
         print(f'City location: {city}')
         print(f'Temperature: {temp} C')
