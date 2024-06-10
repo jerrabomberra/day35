7472687af145f0b4aec3692c0c744b3602965504
Doug Gordon <gordononline@bigpond.com>
2024-06-10 20:58:19 +0700

    including datetime

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import requests
 from ISO3166 import ISO3166
+from datetime import datetime
 
 # abloom is at 13.773648654025774, 100.54110921045502
-
+current_datetime = datetime.now().replace(microsecond=0)
+OEM_endpoint = 'https://api.openweathermap.org/data/2.5/weather'
 API_KEY: str = '8c928036410e6f5ffc2690b3c94b81b8'
-# lat: float = 13.7736
-lat = 13.773648654025774
-# long: float = 100.5411
-long = 100.54110921045502
-URL = f'https://api.openweathermap.org/data/2.5/\
-weather?lat={lat}&lon={long}&appid={API_KEY}&units=metric'
 
-response = requests.get(URL)
+weather_params = {
+    "lat": 13.773648654025774,
+    "lon": 100.54110921045502,
+    "appid": API_KEY,
+    "units": "metric",
+}
 
+response = requests.get(OEM_endpoint, params=weather_params)
 
 def print_weather():
     if response.status_code == 200:
@@ -23,11 +25,12 @@ def print_weather():
         desc = data['weather'][0]['description']
         country_code = data['sys']['country']
         country = ISO3166[country_code]
+        print(f'Weather report for: {current_datetime}')
         print(f'Country: {country}')
         print(f'City location: {city}')
         print(f'Temperature: {temp} C')
         print(f'Feels like: {feels_like} C')
-        print(f'Description: {desc}')
+        print(f'Description: {desc.capitalize()}')
     else:
         print('Error fetching weather data')
 
